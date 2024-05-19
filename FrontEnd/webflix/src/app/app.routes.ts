9105752d8a0820a47e46f7b8e1b28c6fc457889d
Gentil Silva <silvagentil.sg@gmail.com>
2024-05-19 19:13:05 -0300

    First commit

---
+++ FrontEnd/webflix/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: MainLayoutComponent
+    },
+    {
+        path: "**",
+        redirectTo: "/home"
+    }
+];
