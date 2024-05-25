af915089a6d7c275303e39136bee4f78010a424e
Gentil Silva <gentil.silva@bwtech.com>
2024-05-25 13:29:36 -0300

    changing app css to scss

--- FrontEnd/webflix/src/app/app.routes.ts
+++ FrontEnd/webflix/src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
 import { RequestsComponent } from './pages/requests/requests.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: MainLayoutComponent,
-        title: "Home"
-    },
-    {
-      path: "requests",
-      component: RequestsComponent,
-      title: "Requests"
-    },
-    {
-        path: "**",
-        redirectTo: "/home",
-        title: "Home"
-    },
+  {
+    path: "",
+    component: MainLayoutComponent,
+    title: "Home"
+  },
+  {
+    path: "requests",
+    component: RequestsComponent,
+    title: "Requests"
+  },
+  {
+    path: "**",
+    redirectTo: "/home",
+    title: "Home"
+  },
 ];
