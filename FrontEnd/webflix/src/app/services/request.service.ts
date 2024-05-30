38ecfc2a9e697d02bdb5134a727c840b677d435b
CamilyVitoriaSB <camilyvihh6@gmail.com>
2024-05-30 20:45:24 -0300

    feat:signup page criada

--- FrontEnd/webflix/src/app/services/request.service.ts
+++ FrontEnd/webflix/src/app/services/request.service.ts
@@ -13,6 +13,9 @@ export class RequestService {
     {id: '3', titulo: "titulo_tres", genero: "genero_tres", lancamento: "12/05/2023", diretor: "diretor_tres", estudio: "estudio_tres"},
     {id: '4', titulo: "titulo_quatro", genero: "genero_quatro", lancamento: "12/05/2023", diretor: "diretor_quatro", estudio: "estudio_quatro"},
   ]
+  users: DataItem[] = [
+    {id: '1', nome: "Camily", email: "camily@email.com", senha: "teste"}
+  ]
 
   constructor() { }
 
@@ -22,5 +25,9 @@ export class RequestService {
 
   setFilme(filme: DataItem) {
     this.filmes["push"](filme);
+  }
+  
+  setUser(users: DataItem){
+    this.users["push"](users);
   }
 }
